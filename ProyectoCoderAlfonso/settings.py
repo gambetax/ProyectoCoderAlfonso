0bc1779b38704b50f6f00bc35e170bd2adf240d4
Mariano Alfonso <mar.fer.alf@gmail.com>
2022-09-18 16:42:04 -0300

    modificada configuración de db a POSTGRESQL

--- ProyectoCoderAlfonso/settings.py
+++ ProyectoCoderAlfonso/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+import psycopg2
 
 import dj_database_url
 
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-s5&z$hd1+k#2uqh9+-celd%#-b%@$=^s@bc2r&j)qq&qov5ak-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aqueous-sands-40930.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['aqueous-sands-40930.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
@@ -81,12 +82,24 @@ WSGI_APPLICATION = 'ProyectoCoderAlfonso.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proyectocoderalfonso',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
